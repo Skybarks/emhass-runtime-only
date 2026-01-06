6283df21c1b31de3b6acd8d8f3afffc6cd2ac2d4
Skybarks <Skybarks@users.noreply.github.com>
2026-01-06 15:02:05 +0100

    Update run.sh with inverter and battery configurations
    Added configuration options for hybrid inverter and battery settings.

--- run.sh
+++ run.sh
@@ -7,15 +7,31 @@ unset HASSIO_TOKEN
 export SUPERVISOR_TOKEN=""
 export HASSIO_TOKEN=""
 
-cat > /data/config.json <<'JSON'
+cat > /share/config.json <<'JSON'
 {
   "data_path": "/share",
   "log_level": "INFO",
-  "retrieve_hass_conf": {
-    "method": "skip"
-  }
+
+  "inverter_is_hybrid": true,
+  "inverter_ac_output_max": 10000,
+  "inverter_ac_input_max": 10000,
+  "inverter_efficiency_dc_ac": 1,
+  "inverter_efficiency_ac_dc": 1,
+
+  "set_use_battery": true,
+  "battery_nominal_energy_capacity": 9900,
+  "battery_discharge_power_max": 6000,
+  "battery_charge_power_max": 6000,
+  "battery_discharge_efficiency": 0.95,
+  "battery_charge_efficiency": 0.95,
+  "battery_minimum_state_of_charge": 0.10,
+  "battery_maximum_state_of_charge": 1.00,
+  "battery_target_state_of_charge": 0.80,
+
+  "compute_curtailment": false
 }
 JSON
+
 
 
 exec uvicorn emhass.web_server:app \
